a3604105b362e02410a16b730de4fcee5c7d92f0
Jeremiah <jeremherring@gmail.com>
2024-06-22 14:11:24 -0700

    push a little auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Nav from "../components/navbar"
 import "./globals.css";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn
+} from '@clerk/nextjs';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +22,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className="min-h-screen">
+          <SignedIn>
+            <Nav />
+            <div className="flex justify-center items-center p-24">{children}</div>
+          </SignedIn>
+          <SignedOut>
+            <RedirectToSignIn />
+          </SignedOut>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
