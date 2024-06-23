af638f4ff0b193e7380b74706bd83184be348dc6
Jonathan Herring <124759864+jonathan-herring@users.noreply.github.com>
2024-06-22 20:07:24 -0700

    landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import React from 'react';
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <section className="text-center py-20">
+        <h1 className="text-4xl font-bold text-white mb-4">Welcome to ResumePool</h1>
+        <p className="text-lg text-gray-200 mb-8">See how your resume stacks up against others by applying to zero-stakes mock jobs.</p>
+        <Link href="/jobs">
+          <button className="bg-blue-600 text-white px-6 py-3 font-semibold rounded hover:bg-blue-700 transition duration-300">See Job Postings</button>
+        </Link>
+      </section>
   )
 }
